adaa38258b1c20c5fe39631c7e02839bcacf2814
wlgur769 <h01047920825@gmail.com>
2026-06-16 11:18:17 +0900

    Add files via upload

---
+++ js/lib.js
@@ -0,0 +1,9 @@
+// 자바스크립트 파일 lib.js
+function over(obj) 
+        {
+            obj.src = "image/banan.png"    
+        }
+        function out(obj) 
+        {
+            obj.src = "image/apple.png"    
+        }
